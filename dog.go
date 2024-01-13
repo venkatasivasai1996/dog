40923ead5a33a32dd48308c65450dd6271f442d5
venkatasivasai1996 <nadipeneni.siva@gmail.com>
2024-01-13 16:56:49 -0600

    dog changes

---
+++ dog.go
@@ -0,0 +1,8 @@
+package dog
+
+import "strings"
+
+func WhenGrownUP(s string) string {
+     return "When the puppy grows up it says: " + strings.ToUpper(s)
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/venkatasivasai1996/dog
+
+go 1.21.1
